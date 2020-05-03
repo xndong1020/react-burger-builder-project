7665cfe4ecf83674e7cf45ea00d495d77b28d0e7
Nicole Dong <xndong1020@gmail.com>
2020-05-03 16:38:06 +1000

    Adding a Hamburger Icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.jsx
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import classes from './DrawerToggle.module.css';
+
 const DrawerToggle = props => {
-  return <div onClick={props.clicked}>MENU</div>;
+  return (
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        {/* Here needs to hold three div elements because of the assigned css styling */}
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default DrawerToggle;
