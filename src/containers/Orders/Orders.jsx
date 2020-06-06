3eb32ac25f82d0ec345c740c3b92e3db0811c4e7
Nicole Dong <xndong1020@gmail.com>
2020-06-06 16:51:22 +1000

    Fetching Orders

--- src/containers/Orders/Orders.jsx
+++ src/containers/Orders/Orders.jsx
@@ -1,16 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import Order from '../../components/Order/Order'
+import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../components/withErrorHandler/withErrorHandler';
 
 export class Orders extends Component {
-    render() {
-        return (
-            <div>
-                <Order />
-                <Order />
-            </div>
-        )
-    }
+  state = {
+    orders: [],
+    loading: true
+  };
+  componentDidMount() {
+    axios
+      .get('/orders.json')
+      .then(res => {
+        console.log('Order data', res.data);
+        let fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch(err => {
+        this.setState({ loading: false });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Order />
+        <Order />
+      </div>
+    );
+  }
 }
 
-export default Orders
+export default withErrorHandler(Orders);
